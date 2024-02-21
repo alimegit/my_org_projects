6fc3300287cd75ae7dcbdcb316a334612c41a64f
Ali <alisherurinov@68@gmail.com>
2024-02-21 22:41:07 +0500

    http package va fontlar qo'shildi

---
+++ lib/utils/app_text_style.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'appcolors.dart';
+
+class AppTextStyle {
+  //Inter
+  static const TextStyle condensedBold = TextStyle(
+    fontFamily: "Condensed",
+    fontWeight: FontWeight.w200,
+    fontSize: 14,
+    color: AppColors.c_2A3256,
+  );
+  static const TextStyle condensedLight = TextStyle(
+    fontFamily: "Condensed",
+    fontWeight: FontWeight.w300,
+    fontSize: 14,
+    color: AppColors.c_2A3256,
+  );
+  static const TextStyle condensedRegular = TextStyle(
+    fontFamily: "Condensed",
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: AppColors.c_2A3256,
+  );
+  static const TextStyle condensedMedium = TextStyle(
+    fontFamily: "Condensed",
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: AppColors.c_2A3256,
+  );
+
+  static const TextStyle interSemiBold = TextStyle(
+    fontFamily: "Condensed",
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    color: AppColors.c_2A3256,
+  );
+}
