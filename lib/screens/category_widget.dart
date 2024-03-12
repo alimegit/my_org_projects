cb4aafaadeac68e0e41e74b57bfd9daf2ddad8db
Ali <alisherurinov@68@gmail.com>
2024-03-12 15:54:31 +0500

    Uyga Vazifa tugadi:)

--- lib/screens/category_widget.dart
+++ lib/screens/category_widget.dart
@@ -1,42 +1,79 @@
+import 'package:default_project/utils/app_images.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-import '../utils/app_images.dart';
+import '../utils/appcolors.dart';
 
-class Category extends SliverPersistentHeaderDelegate {
+
+class CategoriesFoodView extends SliverPersistentHeaderDelegate {
+  final VoidCallback onTap;
+
+  CategoriesFoodView({
+    required this.onTap,
+  });
+
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    List<CategoryModel> list = [
+      CategoryModel(image: AppImages.badbage, title: 'Vegetables'),
+      CategoryModel(image: AppImages.badbage, title: 'Fruit'),
+      CategoryModel(image: AppImages.badbage, title: 'Spice'),
+      CategoryModel(image: AppImages.badbage, title: 'Ingridients'),
+      CategoryModel(image: AppImages.badbage, title: 'Side Dishes'),
+    ];
+    int active = 0;
+    return Container(
+      height: 100,
+      color: AppColors.white,
+      // margin:EdgeInsets.only(left: 24.w,right: 24.w),
+      padding: EdgeInsets.only(top: 16.h, left: 24.w, right: 24.w),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(20.r),
-              ),
-              child: Center(
-                child: SvgPicture.asset(AppImages.badbage),
-              ),
-            ),
-
+          ...List.generate(list.length, (index) {
+            return Column(
+              children: [
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(16.r),
+                    child: Image.asset(
+                      list[index].image,
+                      width: 40.w,
+                      height: 40.h,
+                      fit: BoxFit.cover,
+                    )),
+                Text(
+                  list[index].title,
+                  style: TextStyle(
+                      color: AppColors.black.withOpacity(0.8), fontSize: 12.sp),
+                )
+              ],
+            );
+          })
         ],
       ),
     );
   }
 
   @override
-  double get maxExtent => 67.h;
+  double get maxExtent => 100.0;
 
   @override
-  // TODO: implement minExtent
-  double get minExtent => 67.h;
+  double get minExtent => 100.0;
 
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    // TODO: implement shouldRebuild
-    throw UnimplementedError();
+    return true;
   }
+}
+
+class CategoryModel {
+  final String image;
+  final String title;
+
+  CategoryModel({
+    required this.image,
+    required this.title,
+  });
 }
--- lib/screens/hello_screen.dart
+++ lib/screens/hello_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:default_project/screens/search.dart';
 import 'package:default_project/utils/appcolors.dart';
 import 'package:flutter/material.dart';
@@ -5,106 +6,249 @@ import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import '../utils/app_images.dart';
-class HelloScreen extends StatelessWidget {
+import 'category_widget.dart';
+
+class HelloScreen extends StatefulWidget {
   const HelloScreen({super.key});
+
+  @override
+  State<HelloScreen> createState() => _HelloScreenState();
+}
+
+class _HelloScreenState extends State<HelloScreen> {
+  final ScrollController scrollController = ScrollController();
+
+  bool isGreen = false;
+
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      if (scrollController.position.pixels > 250) {
+        setState(() {
+          isGreen = true;
+        });
+      } else {
+        setState(() {
+          isGreen = false;
+        });
+      }
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion(
-      value: const  SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      value: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent, statusBarIconBrightness: Brightness.light),
       child: Scaffold(
         backgroundColor: Colors.white,
         body: CustomScrollView(
           slivers: [
-           SliverAppBar(
-             pinned: false,
-             backgroundColor: Colors.white,
-             expandedHeight: 82.h,
-             actions: [
-               InkWell(
-                 borderRadius: BorderRadius.circular(15.r),
-                 onTap: () {},
-                 child: Container(
-                   decoration: BoxDecoration(
-
-                     border: Border.all(
-                       color: AppColors.c_F9F3D0,
-                       width: 2,
-                     ),
-                     color: Colors.white,
-                     borderRadius: BorderRadius.circular(11.r),
-                   ),
-                   child: Center(
-                       child: SvgPicture.asset(
-                     AppImages.nofication,
-                     width: 30,
-                   ),),
-                 ),
-               )
-             ],
-             flexibleSpace: FlexibleSpaceBar(
-               background: Padding(
-                 padding:  EdgeInsets.symmetric(horizontal: 24.w),
-                 child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                     SizedBox(
-                       height: 47.h,
-                     ),
-                     Row(
-                       children: [
-                         Text(
-                           "Hello,",
-                           style: TextStyle(
-                               color: Colors.black, fontWeight: FontWeight.w300, fontSize: 16.sp),
-                         ),
-                         Text(
-                           "Fahmi",
-                           style: TextStyle(
-                               color: Colors.orange, fontWeight: FontWeight.w700, fontSize: 16.sp),
-                         ),
-                       ],
-                     ),
-                     Text(
-                       "Find the right one \nfor a healthy body",
-                       style: TextStyle(
-                           color: Colors.black, fontSize: 16.sp, fontWeight: FontWeight.w700),
-                     ),
-                   ],
-                 ),
-               ),
-             ),
-           ),
-           SliverToBoxAdapter(child: SizedBox(height: 15.h, width: 0.w,),),
+            SliverAppBar(
+              pinned: true,
+              backgroundColor: Colors.white,
+              expandedHeight: 82.h,
+              actions: [],
+              flexibleSpace: FlexibleSpaceBar(
+                background: Padding(
+                  padding: EdgeInsets.only(left: 24.w, right: 24.w),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 47.h,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Hello,",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 16.sp),
+                              ),
+                              Text(
+                                "Fahmi",
+                                style: TextStyle(
+                                    color: Colors.orange,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 16.sp),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Find the right one \nfor a healthy body",
+                            style: TextStyle(
+                                color: Colors.black, fontSize: 16.sp, fontWeight: FontWeight.w700),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      SizedBox(
+                        height: 35.h,
+                        width: 35.w,
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(15.r),
+                          onTap: () {},
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: AppColors.c_F9F3D0,
+                                width: 2,
+                              ),
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(11.r),
+                            ),
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SvgPicture.asset(
+                                  AppImages.nofication,
+                                  width: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 15.h,
+                width: 0.w,
+              ),
+            ),
             SliverPersistentHeader(
+              pinned: true,
               delegate: Search(),
             ),
-            SliverToBoxAdapter(child: SizedBox(height: 15.h, width: 0.w,),),
             SliverToBoxAdapter(
-              child: Row(
-                children: [
-                  Text(
-                    "Category",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16.sp,
-
-                      fontWeight: FontWeight.w700,
+              child: SizedBox(
+                height: 15.h,
+                width: 0.w,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(left: 24.w, right: 24.w),
+                child: Row(
+                  children: [
+                    Text(
+                      "Category",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  ),
-               const  Spacer(),
-                  Text(
-                    "Show All",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w300,
+                    const Spacer(),
+                    Text(
+                      "Show All",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w300,
+                      ),
                     ),
+                  ],
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 10.h,
+              ),
+            ),
+            SliverPersistentHeader(pinned: true, delegate: CategoriesFoodView(onTap: () {})),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(left: 24.w),
+                child: Text(
+                  "Best Deal",
+                  style:
+                      TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 18.sp),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 10.h,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(right: 24.w, left: 24.w),
+                child: Container(
+                  height: 150.h,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16.r),
+                    color: Colors.orange,
                   ),
-                ],
+                ),
               ),
             ),
             SliverToBoxAdapter(child: SizedBox(height: 10.h,),),
-            // SliverPersistentHeader(delegate: Category(),)
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(left: 24.w, right: 24.w),
+                child: Row(
+                  children: [
+                    Text(
+                      "Best Price",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const Spacer(),
+                    Text(
+                      "Show All",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(child: SizedBox(height: 10.h,),),
+            SliverGrid(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return Padding(
+                  padding:  EdgeInsets.only(left: 24.w,right: 24.w),
+                  child: Container(
+                    height: 180.h,
+
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black,width: 1)
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(AppImages.chick,fit: BoxFit.cover,),
+                      ],
+                    ),
+                  ),
+                );
+              },
+                childCount: 100,
+              ),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 0.89,
+              ),
+            ),
           ],
         ),
       ),
