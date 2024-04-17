95d30917131bda26bc0ccf8b12957da5f693eaac
Ali <alisherurinov@68@gmail.com>
2024-04-18 02:16:00 +0500

    Tamom!☺

--- lib/getx/puzzle_controller.dart
+++ lib/getx/puzzle_controller.dart
@@ -1,89 +1,92 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../screen/puzzle/widgets/dialog.dart';
-
 class PuzzleController extends GetxController {
-  var gridSize = 4;
-  RxList<int> tiles = List<int>.generate(16, (index) => index).obs;
+  int gridSize = 4;
+  RxList<int> tiles = List<int>.generate(16, (index) => index + 1).obs;
   Timer? timer;
-  final RxBool gameEnded = false.obs;
-  final RxBool dialogShown = false.obs;
-  final elapsedTime = '00:10'.obs; // O'yin uchun qolgan vaqt
-  final moveCount = 0.obs; // Qancha harakat qilingan
+  RxBool gameEnded = false.obs;
+  RxInt elapsedTime = 0.obs;
+  RxInt moveCount = 0.obs;
 
   @override
   void onInit() {
     super.onInit();
-    shuffleTiles();
-    startTimer(); // Timer boshlash
   }
 
   void startTimer() {
-    int totalSeconds = 10;
     timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      int currentSecond = totalSeconds - timer.tick;
-      if (currentSecond <= 0) {
-        gameEnded.value = true;
-        checkEndGame();
-        timer.cancel();
-      } else {
-        elapsedTime.value = formatTime(currentSecond);
-      }
+      elapsedTime.value++;
     });
   }
 
-  String formatTime(int seconds) {
-    int minutes = seconds ~/ 60;
-    int remainingSeconds = seconds % 60;
-    return '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
+  void shuffleTiles() {
+    var random = Random();
+    tiles.shuffle(random);
+    tiles.refresh();
+    if (isSolved()) {
+      shuffleTiles();
+    }
   }
 
-  void checkEndGame() {
-    if (gameEnded.value && !dialogShown.value) {
-      dialogShown.value = true;
-      Get.dialog(const EndGameDialog(), barrierDismissible: false);
+  bool isSolved() {
+    for (int i = 0; i < tiles.length - 1; i++) {
+      if (tiles[i] != i + 1) return false;
+    }
+    return true;
+  }
+
+  void moveTile(int index) {
+    int zeroIndex = tiles.indexOf(16);
+    if (zeroIndex == index) return;
+
+    int row = index ~/ gridSize;
+    int column = index % gridSize;
+    int zeroRow = zeroIndex ~/ gridSize;
+    int zeroColumn = zeroIndex % gridSize;
+
+    if ((row == zeroRow && (column == zeroColumn + 1 || column == zeroColumn - 1)) ||
+        (column == zeroColumn && (row == zeroRow + 1 || row == zeroRow - 1))) {
+      tiles[zeroIndex] = tiles[index];
+      tiles[index] = 16;
+      moveCount.value++;
+      tiles.refresh();
+
+      if (isSolved()) {
+        gameEnded.value = true;
+        timer?.cancel();
+        showCongratulationsDialog();
+      }
     }
   }
 
   void resetGame() {
     shuffleTiles();
     gameEnded.value = false;
-    dialogShown.value = false;
+    elapsedTime.value = 0;
     moveCount.value = 0;
     timer?.cancel();
     startTimer();
   }
-  void startGame() {
-    resetGame(); // Mavjud reset funksiyasini chaqiradi o'yinni yangilash uchun
-  }
 
-  void moveTile(int index) {
-    int zeroIndex = tiles.indexOf(0);  // 0 elementining indeksini topish
-    if (zeroIndex == index) return;  // Agar bosilgan element 0 elementi bo'lsa, hech narsa qilmaymiz
-
-    int row = index ~/ gridSize;  // Bosilgan elementning qatori
-    int column = index % gridSize;  // Bosilgan elementning ustuni
-
-    int zeroRow = zeroIndex ~/ gridSize;  // 0 elementining qatori
-    int zeroColumn = zeroIndex % gridSize;  // 0 elementining ustuni
-
-    // Faqat yonma-yon joylashgan elementlarni almashtirish
-    if ((row == zeroRow && (column == zeroColumn + 1 || column == zeroColumn - 1)) ||
-        (column == zeroColumn && (row == zeroRow + 1 || row == zeroRow - 1))) {
-      tiles[zeroIndex] = tiles[index];
-      tiles[index] = 0;
-      tiles.refresh();  // UI yangilash
-
-      moveCount.value++;  // Harakatlar sonini oshirish
-    }
-  }
-
-
-  void shuffleTiles() {
-    var random = Random();
-    tiles.shuffle(random);
-    tiles.refresh(); // Refresh the RxList to update UI
+  void showCongratulationsDialog() {
+    Get.dialog(
+      AlertDialog(
+        title: Text("Tabriklaymiz!"),
+        content: Text("Siz ${elapsedTime.value} soniyada va ${moveCount.value} urinishda o'yinni muvaffaqiyatli tugatdingiz."),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Get.back(); // Dialogni yopish
+              resetGame();
+            },
+            child: Text('Yana oynash'),
+          ),
+        ],
+      ),
+      barrierDismissible: false,
+    );
   }
 }
--- lib/screen/puzzle/puzzle_screen.dart
+++ lib/screen/puzzle/puzzle_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import '../../getx/puzzle_controller.dart';
 
 class PuzzleView extends StatelessWidget {
@@ -7,146 +8,81 @@ class PuzzleView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final PuzzleController controller = Get.put(PuzzleController()); // Ensure controller is correctly instantiated
+    final PuzzleController controller = Get.put(PuzzleController());  // Controller'ni yaratish yoki topish
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('15 Puzzle'),
         backgroundColor: Colors.deepPurple,
+        actions: [
+          IconButton(onPressed: (){
+            controller.shuffleTiles();
+          }, icon: Icon(Icons.shuffle)),
+        ],
       ),
       body: Column(
-        children: [
-          SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Obx(() => Text(
-                  'Time: ${controller.elapsedTime.value}',
-                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                )),
-                Obx(() => Text(
-                  'Moves: ${controller.moveCount.value}',
-                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                )),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  margin: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [Colors.deepPurple[300]!, Colors.deepPurple[100]!],
+          children: [
+          const SizedBox(height: 10),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Obx(() => Text(
+              'Vaqt: ${controller.elapsedTime} s',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )),
+            Obx(() => Text(
+              'Harakatlar: ${controller.moveCount}',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )),
+          ],
+        ),
+      ),
+      Expanded(
+        child: Center(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Obx(() => GridView.builder(
+              itemCount: controller.tiles.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: controller.gridSize, // Bu yerda controller.gridSize ishlatilmoqda
+                  mainAxisSpacing: 3,
+                  crossAxisSpacing: 3
+              ),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () => controller.moveTile(index),
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    decoration: BoxDecoration(
+                      color: controller.tiles[index] == 16 ? Colors.transparent : Colors.deepPurple[400],
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
+                    alignment: Alignment.center,
+                    child: Text(
+                      controller.tiles[index] ==16 ? '' : controller.tiles[index].toString(),
+                      style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
                   ),
-                  child: Obx(() {
-                    return GridView.builder(
-                      itemCount: controller.tiles.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: controller.gridSize,
-                          mainAxisSpacing: 4,
-                          crossAxisSpacing: 4),
-                      itemBuilder: (context, index) {
-                        if (controller.tiles[index] == 0) {
-                          return Container(
-                            decoration: BoxDecoration(
-                              color: Colors.transparent,
-                            ),
-                          );
-                        } else {
-                          return GestureDetector(
-                            onTap: () {
-                              if (!controller.gameEnded.value) {
-                                controller.moveTile(index);
-                              }
-                            },
-                            child: AnimatedContainer(
-                              duration: const Duration(milliseconds: 300),
-                              decoration: BoxDecoration(
-                                color: Colors.deepPurple[400],
-                                borderRadius: BorderRadius.circular(8),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.2),
-                                    blurRadius: 4,
-                                  ),
-                                ],
-                              ),
-                              alignment: Alignment.center,
-                              child: Text(
-                                controller.tiles[index].toString(),
-                                style: const TextStyle(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          );
-                        }
-                      },
-                    );
-                  }),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.deepPurple,
-                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                textStyle: const TextStyle(fontSize: 24),
-              ),
-              onPressed: () {
-                if (controller.gameEnded.value && !controller.dialogShown.value) {
-                  controller.dialogShown.value = true;
-                  showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                      title: const Text('Game Over'),
-                      content: const Text('Would you like to start a new game?'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                            controller.startGame();
-                          },
-                          child: const Text('Yes'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('No'),
-                        ),
-                      ],
-                    ),
-                  );
-                } else {
-                  controller.startGame();
-                }
+                );
               },
-              child: const Text('Start Game'),
-            ),
+            )),
           ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8),
+        child: Obx(() => ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurple,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              textStyle: const TextStyle(fontSize: 24),
+            ),
+            onPressed: controller.gameEnded.value ? null : () => controller.resetGame(),
+            child: const Text('Oyinni boshlash'),
+            )),
+        ),
         ],
       ),
     );
