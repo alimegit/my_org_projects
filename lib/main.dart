318cd1ee086ebdfe310082c93c2d026778f9554f
Ali <alisherurinov@68@gmail.com>
2024-05-02 22:24:41 +0500

    File Error in Dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:default_project/app/app.dart';
-import 'package:default_project/bloc/auth_bloc.dart';
-import 'package:default_project/bloc/auth_event.dart';
-import 'package:default_project/data/local/storage_repository.dart';
-import 'package:default_project/data/repositories/auth_repository.dart';
-import 'package:default_project/routes.dart';
 import 'package:default_project/services/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 Future<void> main(List<String> args) async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
